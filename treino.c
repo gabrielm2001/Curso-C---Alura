fe210f717233dcfa887c9a8802d8afde4ae1c2f6
Gabriel Machado <124816401+gabrielm2001@users.noreply.github.com>
2023-03-14 15:07:15 -0300

    Conclui a  primeiro parte

--- treino.c
+++ treino.c
@@ -1,10 +1,41 @@
+// #include <stdio.h>
+
+// int main(){
+
+//     int numero_secreto = 21;
+
+//     int chute;
+
+//     printf("Chute um numero: ");
+//     scanf("%d", &chute);
+//     printf("O numero chutado foi: %d", chute);
+
+//     return 0;
+
+// }
+
+
+
+
+// Desafio
+
 #include <stdio.h>
 
-int main() {
+int main(){
 
-    printf("Ola, Alura!\n");
+    int number_one;
+    int number_two;
 
-    return 0;
+    printf("Digite o primeiro numero ");
+    scanf("%d", &number_one);
+
+    printf("Digite o segundo numero");
+    scanf("%d", &number_two);
+
+    int result = number_one * number_two;
+
+    printf("O resultado da multiplicacao do numero %d e %d foi: %d", number_one, number_two, result);
+
 }
 
 
